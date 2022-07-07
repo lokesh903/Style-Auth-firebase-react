7e30f7aa602191072e8127c9d8f079035ffe8338
lokesh903 <lavkeshchhatani@gmail.com>
2022-07-07 15:33:52 +0530

    Auth using firebase react in style

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,30 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
     apiKey: "AIzaSyA4dbdm-Nzxe4YOORT934W6C7FtpDS7re0",
     authDomain: "auth-with-style-dev.firebaseapp.com",
     projectId: "auth-with-style-dev",
     storageBucket: "auth-with-style-dev.appspot.com",
     messagingSenderId: "442079702098",
     appId: "1:442079702098:web:1834e5074de934b35dc38b"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+})
 
 
-export const auth = app.auth();
+export const auth = app.auth()
 export default app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
